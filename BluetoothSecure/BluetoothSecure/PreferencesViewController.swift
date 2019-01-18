76817c1eee6bfc82326ada919e6420a72ca2c1d6
Kramarchuk <emailjustforsomecase@gmail.com>
2019-01-19 00:27:11 +0200

    [ADD] PreferencesViewController added

---
+++ BluetoothSecure/BluetoothSecure/PreferencesViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PreferencesViewController.swift
+//  BluetoothSecure
+//
+//  Created by Kramarchuk Kyrylo on 1/19/19.
+//  Copyright © 2019 Kramarchuk Kyrylo. All rights reserved.
+//
+
+import Cocoa
+
+class PreferencesViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
--- BluetoothSecure/BluetoothSecure/ViewController.swift
+++ BluetoothSecure/BluetoothSecure/ViewController.swift
@@ -10,10 +10,15 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    @IBOutlet weak var button: NSButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let colorLayer = CAShapeLayer()
+        colorLayer.masksToBounds = true
+        colorLayer.backgroundColor = NSColor.green.cgColor
+        button.layer?.addSublayer(colorLayer)
     }
 
     override var representedObject: Any? {
@@ -22,6 +27,8 @@ class ViewController: NSViewController {
         }
     }
 
-
+    @IBAction func test(_ sender: Any) {
+        
+    }
 }
 
