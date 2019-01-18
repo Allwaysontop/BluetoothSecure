2abff655d7cd88fa11c3f38f15a243eef85e593e
Kramarchuk <emailjustforsomecase@gmail.com>
2019-01-18 22:35:00 +0200

    initial commit

---
+++ BluetoothSecure/BluetoothSecure/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  BluetoothSecure
+//
+//  Created by Kramarchuk Kyrylo on 1/18/19.
+//  Copyright © 2019 Kramarchuk Kyrylo. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ BluetoothSecure/BluetoothSecure/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BluetoothSecure
+//
+//  Created by Kramarchuk Kyrylo on 1/18/19.
+//  Copyright © 2019 Kramarchuk Kyrylo. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
