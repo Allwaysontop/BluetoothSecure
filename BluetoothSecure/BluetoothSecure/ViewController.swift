7e0642333eac62e7fb2dad23529aa8773c70499b
Kramarchuk <emailjustforsomecase@gmail.com>
2019-01-19 17:07:13 +0200

    [ADD] Status bar icon added

--- BluetoothSecure/BluetoothSecure/ViewController.swift
+++ BluetoothSecure/BluetoothSecure/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: NSViewController {
 
     @IBOutlet weak var button: NSButton!
     
+    private let statusBarItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -19,6 +21,12 @@ class ViewController: NSViewController {
         colorLayer.masksToBounds = true
         colorLayer.backgroundColor = NSColor.green.cgColor
         button.layer?.addSublayer(colorLayer)
+        
+        
+        if let button = statusBarItem.button {
+            button.image = #imageLiteral(resourceName: "status bar icon")
+            constructMenu()
+        }
     }
 
     override var representedObject: Any? {
@@ -26,9 +34,39 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
+    
+    // MARK: - Private
+    private func constructMenu() {
+        let menu = NSMenu()
+        menu.addItem(NSMenuItem(title: "Start monitoring", action: #selector(startMonitoringAction), keyEquivalent: "S"))
+        menu.addItem(NSMenuItem.separator())
+        menu.addItem(NSMenuItem(title: "Stop monitoring", action: #selector(stopMonitoringAction), keyEquivalent: "T"))
+
+        statusBarItem.menu = menu
+    }
+    
+    private func startMonitoringBluetoohConnections() {
+        
+    }
+    
+    private func stopMonitoringBluetoohConnections() {
+        
+    }
+
+    // MARK: - Selectors
+    
+    @objc func startMonitoringAction() {
+        startMonitoringBluetoohConnections()
+    }
+    
+    @objc func stopMonitoringAction() {
+        stopMonitoringBluetoohConnections()
+    }
+    
+    // MARK: - Actions
 
     @IBAction func test(_ sender: Any) {
-        
+        NSApp.terminate(self)
     }
 }
 
