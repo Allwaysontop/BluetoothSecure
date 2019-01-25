9357b1dfbb5ec5c29adfc0133ef5c6a63ef8766e
Kramarchuk <emailjustforsomecase@gmail.com>
2019-01-25 22:13:23 +0200

    [UPD] Architecture created

---
+++ BluetoothSecure/BluetoothSecure/Sources/Business Layer/Model/BluetoothModel.swift
@@ -0,0 +1,9 @@
+//
+//  BluetoothModel.swift
+//  BluetoothSecure
+//
+//  Created by Kramarchuk Kyrylo on 1/25/19.
+//  Copyright © 2019 Kramarchuk Kyrylo. All rights reserved.
+//
+
+import Foundation
