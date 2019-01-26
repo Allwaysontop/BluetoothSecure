ce140fe7edd1f984587dc34bac8b61c24f08de52
Kramarchuk <emailjustforsomecase@gmail.com>
2019-01-26 20:02:14 +0200

    [ADD][DELETE] bluetoothModel and databaseService integrated. Unnecessary removed.

--- BluetoothSecure/BluetoothSecure/Sources/Presentation Layer/Controllers/ViewController.swift
+++ BluetoothSecure/BluetoothSecure/Sources/Presentation Layer/Controllers/ViewController.swift
@@ -7,19 +7,15 @@
 //
 
 import Cocoa
-import IOBluetooth
-import CoreBluetooth
 
 class ViewController: NSViewController {
 
     @IBOutlet weak var button: NSButton!
     
     private let statusBarItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
-    private let manager = CBCentralManager()
     private let statusBarMenu = NSMenu()
-    
-//    var peripheral: CBPeripheral!
-    private let bt = BluetoothDevices()
+    private var bluetoothModel: BluetoothModel!
+    private let databaseService = DatabaseServiceImpl()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,7 +24,8 @@ class ViewController: NSViewController {
         colorLayer.masksToBounds = true
         colorLayer.backgroundColor = NSColor.green.cgColor
         button.layer?.addSublayer(colorLayer)
-        manager.delegate = self
+        
+        bluetoothModel = BluetoothModel(delegate: self, databaseService: databaseService)
         
         if let button = statusBarItem.button {
             button.image = #imageLiteral(resourceName: "status bar icon")
@@ -39,7 +36,7 @@ class ViewController: NSViewController {
 
     override var representedObject: Any? {
         didSet {
-            print("")
+            
         }
     }
     
@@ -58,42 +55,13 @@ class ViewController: NSViewController {
         statusBarItem.menu = statusBarMenu
     }
     
-    class BluetoothDevices {
-        
-        func register() {
-            IOBluetoothDevice.register(forConnectNotifications: self, selector: #selector(connected))
-        }
-        
-        func pairedDevices() {
-            print("Bluetooth devices:")
-            guard let devices = IOBluetoothDevice.pairedDevices() else {
-                print("No devices")
-                return
-            }
-            
-            for item in devices {
-                if let device = item as? IOBluetoothDevice {
-                    print("Name: \(device.name)")
-                    print("Paired?: \(device.isPaired())")
-                    print("Connected?: \(device.isConnected())")
-                    print("Address String?: \(device.addressString)")
-                }
-            }
-        }
-        
-        @objc func connected(_ sender: IOBluetoothUserNotification) {
-            pairedDevices()
-        }
-    }
-    
     private func startMonitoringBluetoohConnections() {
-        bt.register()
-//        bt.pairedDevices()
-//        manager.scanForPeripherals(withServices: nil, options: nil)
+        bluetoothModel.register()
+        bluetoothModel.startMonitoring()
     }
     
     private func stopMonitoringBluetoohConnections() {
-        
+        bluetoothModel.stopMonitoring()
     }
 
     // MARK: - Selectors
@@ -117,47 +85,13 @@ class ViewController: NSViewController {
     }
 }
 
-extension ViewController: CBCentralManagerDelegate {
+// MARK: - BluetoothModelDelegate
+
+extension ViewController: BluetoothModelDelegate {
     
-    func centralManagerDidUpdateState(_ central: CBCentralManager) {
-        switch central.state {
-        case .poweredOn:
-            print("Bluetooth is On")
-        case .poweredOff:
-            print("Bluetooth is Off")
-        case .resetting:
-            break
-        case .unauthorized:
-            break
-        case .unsupported:
-            break
-        case .unknown:
-            break
-        }
+    func bluetoothNotifier(macAddress: String) {
+        
     }
-    
-//    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
-//
-//        central.stopScan()
-//        self.peripheral = peripheral
-//        central.connect(self.peripheral, options: nil)
-//    }
-//
-//    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
-//        print("")
-//    }
-    
-//    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
-//        print("centralManager didConnect \(peripheral.name)")
-//    }
-//
-//    func centralManager(_ central: CBCentralManager, didFailToConnect peripheral: CBPeripheral, error: Error?) {
-//        print("centralManager didFailToConnect \(peripheral.name), error \(error?.localizedDescription)")
-//    }
-//
-//    func centralManager(_ central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: Error?) {
-//        print("centralManager didDisconnectPeripheral \(peripheral.name)")
-//    }
 }
 
 extension ViewController: NSMenuDelegate {
