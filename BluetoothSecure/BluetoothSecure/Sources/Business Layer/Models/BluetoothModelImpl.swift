7a7dc090872f28b0587db21507bc31ef4594ae25
Kramarchuk <emailjustforsomecase@gmail.com>
2019-02-14 15:00:10 +0200

    [UPD] call delegate method if only arrays in not empty. Spacing changed from 4 to 2

--- BluetoothSecure/BluetoothSecure/Sources/Business Layer/Models/BluetoothModelImpl.swift
+++ BluetoothSecure/BluetoothSecure/Sources/Business Layer/Models/BluetoothModelImpl.swift
@@ -11,116 +11,119 @@ import IOBluetooth
 import CoreBluetooth
 
 protocol BluetoothModelDelegate: class {
-    func bluetoothNotifier(devices: [BluetoothDeviceEntity])
-    func bluetoothNotifierEmpty()
+  func bluetoothNotifier(devices: [BluetoothDeviceEntity])
+  func bluetoothNotifierEmpty()
 }
 
 class BluetoothModelImpl: BluetoothModelType {
-    
-    weak var delegate: BluetoothModelDelegate?
-    private var isMonitoring: Bool = false
-    private var notification: IOBluetoothUserNotification?
-    private let databaseService: DatabaseServiceType
-    
-    // MARK: - Life cycle
-    
-    init(delegate: BluetoothModelDelegate, databaseService: DatabaseServiceType) {
-        self.delegate = delegate
-        self.databaseService = databaseService
+  
+  weak var delegate: BluetoothModelDelegate?
+  private var isMonitoring: Bool = false
+  private var notification: IOBluetoothUserNotification?
+  private let databaseService: DatabaseServiceType
+  
+  // MARK: - Life cycle
+  
+  init(delegate: BluetoothModelDelegate, databaseService: DatabaseServiceType) {
+    self.delegate = delegate
+    self.databaseService = databaseService
+  }
+  
+  // MARK: - Registration for notifications
+  
+  func register() {
+    IOBluetoothDevice.register(forConnectNotifications: self, selector: #selector(connected))
+  }
+  
+  func unRegister() {
+    notification?.unregister()
+  }
+  
+  // MARK: - Main features
+  
+  func fetchCachedDevices() -> [IOBluetoothDevice] {
+    print("Bluetooth devices:")
+    guard let devices = IOBluetoothDevice.pairedDevices() else {
+      print("No devices")
+      return []
     }
     
-    // MARK: - Registration for notifications
+    var bluetoothDevices: [IOBluetoothDevice] = []
     
-    func register() {
-        IOBluetoothDevice.register(forConnectNotifications: self, selector: #selector(connected))
+    for item in devices {
+      if let device = item as? IOBluetoothDevice {
+        bluetoothDevices.append(device)
+        print("Name: \(device.name)")
+        print("Paired?: \(device.isPaired())")
+        print("Connected?: \(device.isConnected())")
+        print("Address String?: \(device.addressString)")
+      }
     }
     
-    func unRegister() {
-        notification?.unregister()
+    return bluetoothDevices
+  }
+  
+  func addToTrusted(devices: [BluetoothDeviceEntity]) {
+    databaseService.save(devices: devices)
+  }
+  
+  func quickAddPairedToTrusted() {
+    guard let paired = achievePairedDevices() else {
+      return
     }
+    addToTrusted(devices: paired)
+  }
+  
+  func achievePairedDevices() -> [BluetoothDeviceEntity]? {
+    let bluetoothDevicesIOPaired = fetchCachedDevices().filter({ $0.isPaired() })
+    let paired = bluetoothDevicesIOPaired.map({ BluetoothDeviceEntity.init(bluetoothDeviceIO: $0) })
+    return paired
+  }
+  
+  func showTrustedDevices() -> [BluetoothDeviceEntity] {
+    return databaseService.fetchAll()
+  }
+  
+  func deleteAllTrustedDevices() {
+    databaseService.deleteAll()
+  }
+  
+  // MARK: - Monitoring
+  
+  func startMonitoring() {
+    isMonitoring = true
+  }
+  
+  func stopMonitoring() {
+    isMonitoring = false
+  }
+  
+  // MARK: - Selectors
+  
+  @objc func connected(_ sender: IOBluetoothUserNotification) {
+    self.notification = sender
     
-    // MARK: - Main features
-    
-    func fetchCachedDevices() -> [IOBluetoothDevice] {
-        print("Bluetooth devices:")
-        guard let devices = IOBluetoothDevice.pairedDevices() else {
-            print("No devices")
-            return []
-        }
-        
-        var bluetoothDevices: [IOBluetoothDevice] = []
-
-        for item in devices {
-            if let device = item as? IOBluetoothDevice {
-                bluetoothDevices.append(device)
-                print("Name: \(device.name)")
-                print("Paired?: \(device.isPaired())")
-                print("Connected?: \(device.isConnected())")
-                print("Address String?: \(device.addressString)")
-            }
-        }
-        
-        return bluetoothDevices
+    if isMonitoring {
+      checkWithTrustedDevices()
     }
+  }
+  
+  // MARK: - Private
+  
+  private func checkWithTrustedDevices() {
+    let trustedDevices = databaseService.fetchAll()
     
-    func addToTrusted(devices: [BluetoothDeviceEntity]) {
-        databaseService.save(devices: devices)
+    let bluetoothDevicesIO = fetchCachedDevices()
+    if bluetoothDevicesIO.isEmpty {
+      delegate?.bluetoothNotifierEmpty()
+      return
     }
+    let cachedDevices = bluetoothDevicesIO.map({ BluetoothDeviceEntity.init(bluetoothDeviceIO: $0) })
     
-    func quickAddPairedToTrusted() {
-        guard let paired = achievePairedDevices() else {
-            return
-        }
-        addToTrusted(devices: paired)
+    let notTrusted = Array(Set<BluetoothDeviceEntity>(cachedDevices).subtracting(Set(trustedDevices)))
+    
+    if !notTrusted.isEmpty {
+      delegate?.bluetoothNotifier(devices: notTrusted)
     }
-    
-    func achievePairedDevices() -> [BluetoothDeviceEntity]? {
-        let bluetoothDevicesIOPaired = fetchCachedDevices().filter({ $0.isPaired() })
-        let paired = bluetoothDevicesIOPaired.map({ BluetoothDeviceEntity.init(bluetoothDeviceIO: $0) })
-        return paired
-    }
-    
-    func showTrustedDevices() -> [BluetoothDeviceEntity] {
-        return databaseService.fetchAll()
-    }
-    
-    func deleteAllTrustedDevices() {
-        databaseService.deleteAll()
-    }
-    
-    // MARK: - Monitoring
-    
-    func startMonitoring() {
-        isMonitoring = true
-    }
-    
-    func stopMonitoring() {
-        isMonitoring = false
-    }
-    
-    // MARK: - Selectors
-    
-    @objc func connected(_ sender: IOBluetoothUserNotification) {
-        self.notification = sender
-        
-        if isMonitoring {
-            checkWithTrustedDevices()
-        }
-    }
-    
-    // MARK: - Private
-    
-    private func checkWithTrustedDevices() {
-        let trustedDevices = databaseService.fetchAll()
-        
-        let bluetoothDevicesIO = fetchCachedDevices()
-        if bluetoothDevicesIO.isEmpty {
-            delegate?.bluetoothNotifierEmpty()
-            return
-        }
-        let cachedDevices = bluetoothDevicesIO.map({ BluetoothDeviceEntity.init(bluetoothDeviceIO: $0) })
-        
-        let notTrusted = Array(Set<BluetoothDeviceEntity>(cachedDevices).subtracting(Set(trustedDevices)))
-        delegate?.bluetoothNotifier(devices: notTrusted)
-    }
+  }
 }
